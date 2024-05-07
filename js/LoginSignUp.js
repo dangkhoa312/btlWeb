abacb00f5edbdcf74751531846a90510272a12b5
dangkhoa312 <156033710+dangkhoa312@users.noreply.github.com>
2024-05-07 18:02:58 +0700

    Delete js directory

--- js/LoginSignUp.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $("#btt-open").click(function(){
-        $("#mymodal").modal();
-        $("#registerForm").css("display", "none");
-        $("#logIn").css("display", "block");
-        
-    })
-});
-
